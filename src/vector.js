2d08233c7c375e2e548f231ed921a0200d45295f
Matvii Hodovaniuk <matvii@hodovani.uk>
2021-04-08 19:50:13 +0300

    Add distanceTo to Vector

--- src/vector.js
+++ src/vector.js
@@ -3,4 +3,7 @@ export class Vector {
         this.x = x || 0;
         this.y = y || 0;
     }
+    distanceTo(vector) {
+        return Math.sqrt((this.x - vector.x) * (this.x - vector.x) + (this.y - vector.y) * (this.y - vector.y));
+    }
 }
