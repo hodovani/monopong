d00c5a4a78d82bfbbf7e30ce7016b56c6a6bb092
Matvii Hodovaniuk <matvii@hodovani.uk>
2021-04-08 19:50:48 +0300

    Add draw, move methods to Ball class

--- src/ball.js
+++ src/ball.js
@@ -4,5 +4,16 @@ export class Ball {
     constructor(position, velocity) {
         this.position = position;
         this.velocity = velocity || new Vector();
+        this.r = 10;
+    }
+    move(){
+        this.position.x += this.velocity.x;
+        this.position.y += this.velocity.y;
+    }
+    draw(ctx){
+        ctx.beginPath();
+        ctx.fillStyle = '#f0f';
+        ctx.arc(this.position.x, this.position.y, this.r, 0, 2 * Math.PI, false);
+        ctx.fill();
     }
 }
