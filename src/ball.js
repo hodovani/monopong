cffe15f9b5f730817532a479404f676af4a2965d
Matvii Hodovaniuk <matvii@hodovani.uk>
2021-04-09 22:40:06 +0300

    Add ball size

--- src/ball.js
+++ src/ball.js
@@ -4,7 +4,7 @@ export class Ball {
     constructor(position, velocity) {
         this.position = position;
         this.velocity = velocity || new Vector();
-        this.r = 10;
+        this.radius = 10;
     }
     move(){
         this.position.x += this.velocity.x;
@@ -13,7 +13,7 @@ export class Ball {
     draw(ctx){
         ctx.beginPath();
         ctx.fillStyle = '#f0f';
-        ctx.arc(this.position.x, this.position.y, this.r, 0, 2 * Math.PI, false);
+        ctx.arc(this.position.x, this.position.y, this.radius, 0, 2 * Math.PI, false);
         ctx.fill();
     }
 }
